65062bc63ff83c4711aa9398c684f908502a9ffa
forlyby <forlyby@naver.com>
2025-09-13 20:59:33 +0900

    Feat: 구글 로그인 api구현

---
+++ src/main/java/com/mobi/mobi/auth/service/OauthService.java
@@ -0,0 +1,109 @@
+package com.mobi.mobi.auth.service;
+
+import com.mobi.mobi.auth.dto.GoogleLoginResponseDTO;
+import com.mobi.mobi.config.jwt.JwtTokenProvider;
+import com.mobi.mobi.member.entity.Member;
+import com.mobi.mobi.member.entity.enums.LoginType;
+import com.mobi.mobi.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OauthService {
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final WebClient webClient;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String GOOGLE_CLIENT_ID;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String GOOGLE_CLIENT_SECRET;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String GOOGLE_REDIRECT_URI;
+
+
+    @Transactional
+    public GoogleLoginResponseDTO loginWithGoogle(String code) {
+
+        String decodedCode = URLDecoder.decode(code, StandardCharsets.UTF_8);
+
+        Map<String, Object> tokenResponse = getGoogleAccessToken(decodedCode);
+        String accessToken = (String) tokenResponse.get("access_token");
+        Map<String, Object> userInfo = getGoogleUserInfo(accessToken);
+
+        String email = (String) userInfo.get("email");
+        String name = (String) userInfo.get("name");
+
+        Optional<Member> memberOptional = memberRepository.findByEmail(email);
+        boolean isNewMember = memberOptional.isEmpty();
+        Member member = memberOptional.orElseGet(() -> {
+            Member newMember = Member.builder()
+                    .email(email).username(name).profileImgUrl(null).loginType(LoginType.GOOGLE)
+                    .build();
+            newMember.setIsPrivacyAgreed(true);
+            return memberRepository.save(newMember);
+        });
+
+        String serviceAccessToken = jwtTokenProvider.createToken(member.getId().toString());
+
+        return GoogleLoginResponseDTO.builder()
+                .isNewMember(isNewMember).accessToken(serviceAccessToken).member(member)
+                .build();
+    }
+
+
+    private Map<String, Object> getGoogleAccessToken(String code) {
+        String tokenUri = "https://oauth2.googleapis.com/token";
+
+        log.info("--- 구글에 액세스 토큰 요청 ---");
+        log.info("client_id: {}", GOOGLE_CLIENT_ID);
+        log.info("redirect_uri: {}", GOOGLE_REDIRECT_URI);
+
+        try {
+            return webClient.post()
+                    .uri(tokenUri, uriBuilder -> uriBuilder
+                            .queryParam("code", code)
+                            .queryParam("client_id", GOOGLE_CLIENT_ID)
+                            .queryParam("client_secret", GOOGLE_CLIENT_SECRET)
+                            .queryParam("redirect_uri", GOOGLE_REDIRECT_URI)
+                            .queryParam("grant_type", "authorization_code")
+                            .build())
+                    .retrieve()
+                    .bodyToMono(Map.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            String responseBody = e.getResponseBodyAsString();
+            log.error("!!! 구글 서버 에러 응답: {}", responseBody);
+
+            // 에러 메시지에 구글의 응답을 포함시켜서 다시 던집니다.
+            throw new IllegalStateException("Google API 요청 실패. 응답 Body: " + responseBody, e);
+        }
+    }
+
+
+    private Map<String, Object> getGoogleUserInfo(String accessToken) {
+        String userInfoUri = "https://www.googleapis.com/oauth2/v2/userinfo";
+        return webClient.get()
+                .uri(userInfoUri)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+    }
+}
--- src/main/java/com/mobi/mobi/config/SecurityConfig.java
+++ src/main/java/com/mobi/mobi/config/SecurityConfig.java
@@ -1,15 +1,12 @@
 package com.mobi.mobi.config;
 
-import com.mobi.mobi.auth.jwt.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,64 +14,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AuthenticationConfiguration configuration;
-    private final JWTUtil jwtUtil;
-
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-
-        return configuration.getAuthenticationManager();
-    }
-
-
-    // 시큐리티 필터 메서드
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-
-        // 스프링 시큐리티 jwt 로그인 설정
-
-        // csrf disable 설정
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((auth) -> auth.disable());
-
-        // 폼로그인 형식 disable 설정 => POSTMAN으로 검증할 것임!
-        http
-                .formLogin((auth) -> auth.disable());
-
-        // http basic 인증 방식 disable 설정
-        http
-                .httpBasic((auth -> auth.disable()));
-
-        // 경로별 인가 작업
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/jwt-login", "/jwt-login/", "/jwt-login/login", "/jwt-login/join").permitAll()
-                        .requestMatchers("/jwt-login/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated()
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        // /auth/** API는 인증 없이 누구나 접근 가능하도록 허용
+                        .requestMatchers("/auth/**").permitAll()
+                        // Swagger UI 접근 허용
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        // 그 외 다른 모든 요청은 일단 허용 (나중에 필요에 따라 인증 설정 추가)
+                        .anyRequest().permitAll()
                 );
-
-        // 세션 설정
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        // 새로 만든 로그인 필터를 원래의 (UsernamePasswordAuthenticationFilter)의 자리에 넣음
-        http
-                .addFilterAt(new LoginFilter(authenticationManager(configuration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
-
-        // 로그인 필터 이전에 JWTFilter를 넣음
-        http
-                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
 
 
         return http.build();
-    }
-
-    // BCrypt password encoder를 리턴하는 메서드
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-
-
-        return new BCryptPasswordEncoder();
     }
 }
