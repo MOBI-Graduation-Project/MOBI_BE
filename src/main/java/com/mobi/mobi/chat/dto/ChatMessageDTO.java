692131baa69b4828eff08be8f259fbf18d671b46
forlyby <forlyby@naver.com>
2025-10-26 19:46:25 +0900

    refactor: 채팅내역 프로필이미지 추가

--- src/main/java/com/mobi/mobi/chat/dto/ChatMessageDTO.java
+++ src/main/java/com/mobi/mobi/chat/dto/ChatMessageDTO.java
@@ -12,6 +12,7 @@ public class ChatMessageDTO {
     private Long roomId; // 메시지를 보낼 채팅방 ID
     private Long senderId; // 보낸 사람 ID
     private String senderNickname; // 보낸 사람 닉네임
+    private String senderProfileImgUrl;
     private String content; // 메시지 내용
     private LocalDateTime sentAt; // 보낸 시간
     private boolean isRead; // 읽음 여부 필드
@@ -22,6 +23,7 @@ public class ChatMessageDTO {
         dto.setRoomId(entity.getChatRoom().getId());
         dto.setSenderId(entity.getSender().getId());
         dto.setSenderNickname(entity.getSender().getNickname());
+        dto.setSenderProfileImgUrl(entity.getSender().getProfileImgUrl());
         dto.setContent(entity.getContent());
         dto.setSentAt(entity.getCreatedAt()); // BaseEntity의 createdAt 사용
         dto.setRead(entity.isRead());
